27681e358a196ad2c477aa4d4f341b8534f0fb4a
Tyler Sommer <sommertm@gmail.com>
2014-12-24 06:36:05 -0700

    Updated with latest changes in nice/framework

--- web/index.php
+++ web/index.php
@@ -1,20 +1,23 @@
 <?php
 
-use Nice\Extension\CacheExtension;
-use Nice\Extension\DoctrineKeyValueExtension;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Nice\Application;
 use Nice\Router\RouteCollector;
-use Nice\Extension\DoctrineDbalExtension;
 use Nice\Extension\LogExtension;
+use Nice\Extension\CacheExtension;
+use Nice\Extension\DoctrineDbalExtension;
+use Nice\Extension\DoctrineKeyValueExtension;
+use Nice\Extension\SecurityExtension;
+use Nice\Extension\SessionExtension;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 // Enable Symfony debug error handlers
 Symfony\Component\Debug\Debug::enable();
 
-$app = new Application();
+$app = new Application('dev', true, false);
+$app->appendExtension(new SessionExtension());
 $app->appendExtension(new CacheExtension(array(
     'connections' => array(
         'default' => array(
@@ -49,25 +52,45 @@ $app->appendExtension(new LogExtension(array(
         )
     )
 )));
+$app->appendExtension(new SecurityExtension(array(
+    'firewall' => '/messages',
+    'authenticator' => array(
+        'type' => 'closure'
+    )
+)));
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace('Doctrine\KeyValueStore', __DIR__ . '/../vendor/doctrine/key-value-store/lib');
 
 // Configure your routes
 $app->set('routes', function (RouteCollector $r) {
-    $r->addRoute('GET', '/', function (Application $app, Request $request) {
+    $r->map('/', 'home', function (Application $app) {
         $app->get('logger.default')->debug('This is a test');
+        $url = $app->get('router.url_generator')->generate('hello', array('name' => 'Tyler'));
 
-        return new Response('Hello, world');
+        return new Response('Hello, world. <a href="' . $url . '">Hello, Tyler.</a>');
     });
 
-    $r->addRoute('GET', '/hello/{name}', function (Application $app, Request $request, $name) {
+    $r->map('/last-visit', null, function (Application $app, Request $request) {
+        $session = $request->getSession();
+
+        $lastVisit = $session->get('last-visited', 'Never');
+        $session->set('last-visited', date('Y-m-d'));
+
+        return new Response('You last visited on: ' . $lastVisit);
+    });
+
+    $r->map('/login', null, function (Application $app, Request $request) {
+        return new Response('<form action="login_check" method="post"><input name="user" /><button>Save</button></form>');
+    });
+
+    $r->map('/hello/{name}', 'hello', function (Application $app, Request $request, $name) {
         $cache = $app->get('cache.default');
         $cache->save('last-hello', $name);
 
         return new Response('Hello, ' . $name . '!');
     });
 
-    $r->addRoute('GET', '/last-hello', function (Application $app) {
+    $r->map('/last-hello', null, function (Application $app) {
         $cache = $app->get('cache.default');
         $name = $cache->fetch('last-hello');
 
@@ -78,13 +101,13 @@ $app->set('routes', function (RouteCollector $r) {
         return new Response('Last said hello to: ' . $name);
     });
 
-    $r->addRoute('GET', '/messages', function (Application $app, Request $request) {
+    $r->map('/messages', null, function (Application $app, Request $request) {
         $conn = $app->get('doctrine.dbal.database_connection');
         $results = $conn->executeQuery("SELECT * FROM messages")->fetchAll();
         return new \Symfony\Component\HttpFoundation\JsonResponse($results);
     });
 
-    $r->addRoute('GET', '/make-person/{name}/{age}', function (Application $app, $name, $age) {
+    $r->map('/make-person/{name}/{age}', null, function (Application $app, $name, $age) {
         $em = $app->get('doctrine.key_value.entity_manager');
         $person = new \Example\Person($name, $age);
         $em->persist($person);
@@ -92,7 +115,7 @@ $app->set('routes', function (RouteCollector $r) {
         return new Response('Person added!');
     });
 
-    $r->addRoute('GET', '/person/{name}', function (Application $app, $name) {
+    $r->map('/person/{name}', null, function (Application $app, $name) {
         $em = $app->get('doctrine.key_value.entity_manager');
 
         $person = $em->find('Example\Person', $name);
@@ -102,6 +125,10 @@ $app->set('routes', function (RouteCollector $r) {
 
         return new Response($name . ' is ' . $person->getAge() . ' years old!');
     });
+});
+
+$app->set('security.authenticator', function(Request $request) {
+    return $request->get('user') === 'tom';
 });
 
 // Run the application
